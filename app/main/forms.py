87305d1cde4de7d029255e85d1976626bf3d034e
timesol <vatotechnics@gmail.com>
2019-02-07 08:16:52 +0100

    toogle button

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
-    TextAreaField,SelectField
+    TextAreaField,SelectField,RadioField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
     Length,InputRequired
 from flask_babel import _, lazy_gettext as _l
@@ -11,8 +11,7 @@ from app.models import User
 class TaskForm(FlaskForm):
 	summary= StringField(_l('Summarize your Problem'), validators=[DataRequired()])
 	body= TextAreaField(_l('Descripe your Problem'), validators=[DataRequired()], id='body-add-task')
-	internet= SelectField(_l('Does the Device still have access to the Internet?'), validators=[DataRequired()]
-		,choices=[('no_internet', 'No Internet'),('internet','Internet')])
+	internet=BooleanField(_l('Does the Device still have access to the Internet?'), render_kw={'data-toggle':'toggle','data-on':'Yes' ,'data-off':'No'}, id='internet-toogle')
 	tags= StringField(_l('Tags'), validators=[DataRequired()])
 	city=SelectField(u'City', coerce=int, validators=[InputRequired()])
 	create = SubmitField(_l('Create'))
